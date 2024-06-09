27ee2ae4c9df5f8ede443efa7f526b6103baa6e6
solid0us <blt11797@gmail.com>
2024-06-08 18:07:23 -0700

    Implement React Hook Forms and useMutation To Handle Modal Interaction Better

--- src/app/(dashboard)/dashboard/appointments/_actions/bookAppointment-actions.ts
+++ src/app/(dashboard)/dashboard/appointments/_actions/bookAppointment-actions.ts
@@ -12,21 +12,6 @@ export type FormState<T> = {
   errors: Record<keyof T, string> | undefined;
   fieldValues: T;
 };
-export const bookAppointments = async (
-  prevState: FormState<BookAppointmentForm>,
-  formData: FormData
-): Promise<FormState<BookAppointmentForm>> => {
-  const appointmentId = formData.get("appointmentId") as string;
-  const clientsId = formData.get("clientsId") as string;
-  const visitReasonId = formData.get("visitReasonId") as string;
-  console.log(clientsId);
-  return {
-    errors: undefined,
-    message: "success",
-    fieldValues: {
-      appointmentId,
-      clientsId,
-      visitReasonId,
-    },
-  };
+export const bookAppointments = async (formData: BookAppointmentForm) => {
+  console.log("server action reached!");
 };
